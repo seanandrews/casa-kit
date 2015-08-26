bca94d230ce301cdc2d35bb084b69e4fb1c965a7
Sean Andrews <sandrews@cfa.harvard.edu>
2015-08-26 09:41:04 -0400

    implemented a rolling mean to mitigate variations with spatial scale

--- ck_reweight.py
+++ ck_reweight.py
@@ -3,7 +3,9 @@ import os
 import time
 import sys
 import matplotlib.pyplot as plt
+import pandas as pd
 
+tic = time.time()
 
 # load some data
 visdata = np.load('test_data/blind2_fo.340GHz.vis.npz')
@@ -15,17 +17,42 @@ Im = visdata['Im']
 Wt = visdata['Wt']
 nvis = len(Re)
 
+# compute the phase-center distances
+Ruv = np.sqrt(u**2+v**2)
+
+# sort based on phase-center distances
+Res = Re[np.argsort(Ruv)]
+Ims = Im[np.argsort(Ruv)]
+Wts = Wt[np.argsort(Ruv)]
+rho = Ruv[np.argsort(Ruv)]
+
+# compute a running mean of the visibility profile
+runmean = pd.rolling_mean(Res, 1000, center=True)
+
 # convert weights into uncertainties
-sig = 1./np.sqrt(Wt)
+sig = 1./np.sqrt(Wts)
 
-nclump = 1000
-for i in np.arange(1):
-    sort_ix = np.argsort(np.sqrt((u-u[i])**2 + (v-v[i])**2))
-    Re_clump = (Re[sort_ix])[:nclump]
-    Im_clump = (Im[sort_ix])[:nclump]
-    n, bins, patches = plt.hist(Re_clump, 50, facecolor='green', alpha=0.75)
+plt.plot(rho, Res-runmean, '.r', rho, np.zeros_like(rho), '--b')
+#plt.plot(rho, runmean, 'b')
 
-plt.axis([0.0, 0.15, 0.0, 100.])
+nclump = 100
+Re_scat = np.zeros_like(Re)
+Im_scat = np.zeros_like(Im)
+tic = time.time()
+for i in np.arange(10):
+    uvsep = ((u-u[i])**2 + (v-v[i])**2)
+    uvsep = uvsep[uvsep < 100.]
+    Re_scat[i] = np.std((Re[np.argsort(uvsep)])[1:nclump])
+    Im_scat[i] = np.std((Im[np.argsort(uvsep)])[1:nclump])
+
+toc = time.time()
+#n, bins, patches = plt.hist(sig, 10, normed=1, facecolor='g', alpha=.8)
+#n, bins, patches = plt.hist(Re_scat, 100, normed=1, facecolor='r', alpha=.1)
+#n, bins, patches = plt.hist(Im_scat, 100, normed=1, facecolor='b', alpha=.1)
+#plt.axis([0.0, 0.01, 0.0, 1000.])
 plt.savefig('test.png')
 plt.clf()
 
+#toc = time.time()
+
+print(nvis*(toc-tic)/3600.)
