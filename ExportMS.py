7d8beb8a42d2309f55021b069113a349e15da430
Sean Andrews <sandrews@cfa.harvard.edu>
2015-09-16 14:42:53 -0400

    toss flagged data; complex numbers preferred

--- ExportMS.py
+++ ExportMS.py
@@ -2,15 +2,14 @@ import numpy as np
 
 # .ms file name
 oms_path = 'test_data'
-oms_name = 'testcont'
+oms_name = 'testspec'
 
 # Use CASA table tools to get columns of UVW, DATA, WEIGHT, etc.
 tb.open(oms_path+'/'+oms_name+'.ms')
 data   = tb.getcol("DATA")
-flag   = np.invert(tb.getcol("FLAG"))	# want flagged data == FALSE (avoid)
+flag   = tb.getcol("FLAG")
 uvw    = tb.getcol("UVW")
 weight = tb.getcol("WEIGHT")
-spw    = tb.getcol("DATA_DESC_ID")
 tb.close()
 
 # Get the frequency information
@@ -19,23 +18,27 @@ tb.open(oms_path+'/'+oms_name+'.ms/SPECTRAL_WINDOW')
 freq = np.squeeze(tb.getcell("CHAN_FREQ"))
 tb.close()
 
-# break out the u, v spatial frequencies (in meter units)
+# Get rid of any flagged columns 
+flagged   = np.all(flag, axis=(0, 1))
+unflagged = np.squeeze(np.where(problem_columns == False))
+data   = data[:,:,unflagged]
+weight = weight[:,unflagged]
+uvw    = uvw[:,unflagged]
+
+# Break out the u, v spatial frequencies (in meter units)
 u = uvw[0,:]
 v = uvw[1,:]
 
-# until CASA records spectral-dependence in its weights, assign the same 
-# weight to each spectral channel
+# Assign uniform spectral-dependence to the weights (pending CASA improvements)
 sp_wgt = np.zeros_like(data.real)
 for i in range(nchan): sp_wgt[:,i,:] = weight
 
 # (weighted) average the polarizations
-# note that including the boolean flag here removes the contribution of flagged
-# data from the averages; leaves flagged data in with zero weight (sort of 
-# wasteful, but it should work generically); squeeze for continuum data
-Re = np.squeeze(np.sum(flag*data.real*sp_wgt, axis=0) / np.sum(sp_wgt, axis=0))
-Im = np.squeeze(np.sum(flag*data.imag*sp_wgt, axis=0) / np.sum(sp_wgt, axis=0))
-Wt = np.squeeze(np.sum(flag*sp_wgt, axis=0))
+Re  = np.squeeze(np.sum(data.real*sp_wgt, axis=0) / np.sum(sp_wgt, axis=0))
+Im  = np.squeeze(np.sum(data.imag*sp_wgt, axis=0) / np.sum(sp_wgt, axis=0))
+Vis = Re + 1j*Im
+Wgt = np.squeeze(np.sum(sp_wgt, axis=0))
 
 # output to numpy file
 os.system('rm -rf '+oms_name+'.vis.npz')
-np.savez(oms_name+'.vis', u=u, v=v, Re=Re, Im=Im, Wt=Wt, freq=freq)
+np.savez(oms_name+'.vis', u=u, v=v, Vis=Vis, Wgt=Wgt, freq=freq)
