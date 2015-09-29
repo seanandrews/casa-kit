2972a6c68bd4de0561e9600b692fce4ed640a4b2
Sean Andrews <sandrews@cfa.harvard.edu>
2015-09-29 11:04:58 -0400

    modified export for multi-SPW continuum

--- ExportMS.py
+++ ExportMS.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 # .ms file name
-oms_path = 'test_data'
-oms_name = 'spw12_avg'
+oms_path = '.'
+oms_name = 'continuum'
 
 # Use CASA table tools to get columns of UVW, DATA, WEIGHT, etc.
 tb.open(oms_path+'/'+oms_name+'.ms')
@@ -10,11 +10,12 @@ data   = tb.getcol("DATA")
 flag   = tb.getcol("FLAG")
 uvw    = tb.getcol("UVW")
 weight = tb.getcol("WEIGHT")
+spwid  = tb.getcol("DATA_DESC_ID")
 tb.close()
 
 # Get the frequency information
 tb.open(oms_path+'/'+oms_name+'.ms/SPECTRAL_WINDOW')
-freq = tb.getcell("CHAN_FREQ")
+freq = tb.getcol("CHAN_FREQ")
 tb.close()
 
 # Get rid of any flagged columns 
@@ -37,8 +38,7 @@ Re  = np.sum(data.real*sp_wgt, axis=0) / np.sum(sp_wgt, axis=0)
 Im  = np.sum(data.imag*sp_wgt, axis=0) / np.sum(sp_wgt, axis=0)
 Vis = np.squeeze(Re + 1j*Im)
 Wgt = np.squeeze(np.sum(sp_wgt, axis=0))
-#if (len(freq) == 1): freq = freq[0]
 
 # output to numpy file
 os.system('rm -rf '+oms_name+'.vis.npz')
-np.savez(oms_name+'.vis', u=u, v=v, Vis=Vis, Wgt=Wgt, freq=freq[0])
+np.savez(oms_name+'.vis', u=u, v=v, Vis=Vis, Wgt=Wgt, SPW=spwid, freq=freq)
