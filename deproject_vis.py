0f4752c3380b062f38480f9c8e4a321a86ffa5a4
Sean Andrews <sandrews@cfa.harvard.edu>
2015-12-16 08:26:30 -0500

    less clumsy coordinate transformation

--- deproject_vis.py
+++ deproject_vis.py
@@ -12,17 +12,16 @@ def deproject_vis(data, bins=np.array([0.]), incl=0., PA=0., offx=0., offy=0.,
     offx *= -np.pi/(180.*3600.)
     offy *= -np.pi/(180.*3600.)
 
-    # - change to a deprojected, cylindrical coordinate system
-    thet = np.arctan2(v, u)
-    dmaj = -np.sqrt(u**2+v**2) * np.cos(thet+PAr) * np.cos(inclr)
-    dmin = -np.sqrt(u**2+v**2) * np.sin(thet+PAr)
-    rhop = np.sqrt(dmaj**2 + dmin**2)
+    # - change to a deprojected, rotated coordinate system
+    uprime = (u*np.cos(PAr) + v*np.sin(PAr)) * np.cos(inclr)
+    vprime = -u*np.sin(PAr) + v*np.cos(PAr)
+    rhop = np.sqrt(uprime**2 + vprime**2)
 
     # - phase shifts to account for offsets
-    amp = np.sqrt(vis.real**2 + vis.imag**2)
-    pha = np.arctan2(vis.imag, vis.real)
-    realp = amp*np.cos(pha+2.*np.pi*(offx*u+offy*v))
-    imagp = amp*np.sin(pha+2.*np.pi*(offx*u+offy*v))
+    shifts = np.exp(-2.*np.pi*1.0j*(u*-offx + v*offy))
+    visp = vis*shifts
+    realp = visp.real
+    imagp = visp.imag
 
     # - if requested, return a binned (averaged) representation
     if (bins.size > 1.):
@@ -32,15 +31,22 @@ def deproject_vis(data, bins=np.array([0.]), incl=0., PA=0., offx=0., offy=0.,
         berr = np.zeros_like(bins, dtype='complex')
         for ib in np.arange(len(bins)):
             inb = np.where((rhop >= bins[ib]-bwid) & (rhop < bins[ib]+bwid))
-            bRe, eRemu = np.average(realp[inb], weights=wgt[inb], returned=True)
-            eRese = np.std(realp[inb])
-            bIm, eImmu = np.average(imagp[inb], weights=wgt[inb], returned=True)
-            eImse = np.std(imagp[inb])
-            bvis[ib] = bRe+1j*bIm
-            if (errtype == 'scat'):
-                berr[ib] = eRese+1j*eImse
-            else: berr[ib] = 1./np.sqrt(eRemu)+1j/np.sqrt(eImmu)
-        output = bins, bvis, berr
+            if (len(inb[0]) >= 5):
+                bRe, eRemu = np.average(realp[inb], weights=wgt[inb], 
+                                        returned=True)
+                eRese = np.std(realp[inb])
+                bIm, eImmu = np.average(imagp[inb], weights=wgt[inb], 
+                                        returned=True)
+                eImse = np.std(imagp[inb])
+                bvis[ib] = bRe+1j*bIm
+                if (errtype == 'scat'):
+                    berr[ib] = eRese+1j*eImse
+                else: berr[ib] = 1./np.sqrt(eRemu)+1j/np.sqrt(eImmu)
+            else:
+                bvis[ib] = 0+1j*0
+                berr[ib] = 0+1j*0
+            parser = np.where(berr.real != 0)
+            output = bins[parser], bvis[parser], berr[parser]
         return output       
         
     # - if not, returned the unbinned representation
